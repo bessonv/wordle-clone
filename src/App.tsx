2039dbb3fc7551d5025ea12df6567f9a08a93698
Denis <bessonov94@gmail.com>
2023-08-25 15:40:59 +0300

    add rules

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,19 @@ const defaultWord = 'craft';
 
 function App() {
   return (
-    <GameBoard answerWord={defaultWord}/>
+    <>
+      <header>Wordle-Clone</header>
+      <div className="rules">
+      <p>Welcome to wordle-clone.
+      You have 6 chances to guess a 5-letter word. The color of the letter represent how close your guess to the word.</p>
+      <ul>
+        <li><span className="green">green</span> - letter on the correct spot</li>
+        <li><span className="yellow">yellow</span> - letter is in the word but in the wron spot</li>
+        <li><span className="gray">gray</span> - letter is not in the word.</li>
+      </ul>
+      </div>
+      <GameBoard answerWord={defaultWord}/>
+    </>
   )
 }
 
